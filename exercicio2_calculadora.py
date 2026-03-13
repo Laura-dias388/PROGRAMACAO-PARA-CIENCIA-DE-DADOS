25989158637cf26e356b0c6d9feb2852f3b73d28
Laura <ldiasvitaltoledo8@gmail.com>
2026-03-13 00:47:18 -0300

    fix: corrige erro de digitação em rocket

--- exercicio2_calculadora.py
+++ exercicio2_calculadora.py
@@ -5,4 +5,4 @@ calculator = int(input(f"Qual operação básica você deseja realizar:",
 
 if (calculator == 1):
   print(f"Calculando adição:")
-  int(input())
+  number_one = int(input("Digite o primeiro número:"))
